f8dda0d4edbfa447b91a1b84e00a874ae96fb753
Pqo <p.torosyan@list.ru>
2024-02-18 17:21:23 +0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿System.Console.Clear(); 
+int max = 3;     
+string[] array1 = { "05", "214", "Armenia", ":-), af, 12, 23" };
+Solving(array1, max);
+string[] array2 = { "5065", "-2", "123, an, Pash, 34" };
+Solving(array2, max);
+string[] array3 = { "2321", "32", "Kazan, 932, 2003" };
+Solving(array3, max);
+
+int CountItems(string[] arr, int max)
+{
+    int count = 0;                                      
+    for (int i = 0; i < arr.Length; i++)               
+    {
+        if (arr[i].Length <= max) count++;              
+    }
+    return count;                                       
+}
+string[] NewArray(string[] arr, int max)
+{
+    int newArrayLenght = CountItems(arr, max);         
+    string[] newArray = new string[newArrayLenght];     
+    int newArrayIndex = 0;                              
+    for (int i = 0; i < arr.Length; i++)                
+    {
+        if (arr[i].Length <= max)                       
+        {
+            newArray[newArrayIndex] = arr[i];           
+            newArrayIndex++;                            
+        }
+    }
+    return newArray;                                    
+}
+void PrintArray(string[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write('"');
+        System.Console.Write(array[i]);
+        System.Console.Write('"');
+        if (i < array.Length - 1) System.Console.Write(", ");
+    }
+    System.Console.Write("]");
+}
+void Solving(string[] arr, int max)
+{
+    string[] newArray = NewArray(arr, max);
+    PrintArray(arr);
+    System.Console.Write(" → ");
+    PrintArray(newArray);
+    System.Console.WriteLine();
+}
